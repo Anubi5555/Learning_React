e10d56718bb7963e523fe3720e1950c645378393
Anubi5555 <63111329+Anubi5555@users.noreply.github.com>
2023-04-09 19:01:14 +0000

    functional component-finale

--- try-one-general/src/components/Greet.js
+++ try-one-general/src/components/Greet.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function Greet () 
-{
-    return <h1>Hello BRO!</h1>
-}
+//function Greet () 
+//{
+//    return <h1>Hello BRO!</h1>
+//}
+
+// Better way to do this we can import this as MyComponent
+const Greet = () => <h1>Hello BRO</h1>
 
 export default Greet
+
+// This way we must import this with a name 
+
+ //export const Greet = () => <h1>Hello BRO</h1>
 
 // Functional component 
